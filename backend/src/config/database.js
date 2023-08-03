b79a93b31f12a5db346bb5721d088989ed74f348
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-08-03 19:42:29 -0300

    Migration

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'postgres',
+  password: 'admin',
   database: 'codeburger',
   define: {
     timespamps: true,
